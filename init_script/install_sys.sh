32dd0baba9642a9f6398494b85943975b5d9def9
hyliang96 <hyliang96@hotmail.com>
2019-02-26 19:22:32 +0800

    add install_sys.sh

---
+++ init_script/install_sys.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# git clone
+cd /home/$USER
+git clone https://github.com/hyliang96/system-template.git
+cp /home/$USER/system-template/{*,.*} /home/$USER
+
+git clone https://github.com/hyliang96/shareENV.git /home/$USER/ENV/shareENV
+git clone https://github.com/hyliang96/serverENV.git /home/$USER/ENV/serverENV
+
+# 更换链接
+bash init_script/add_link.sh
+
+zsh
+
+# 安装我的agnoster
+rm ~/.antigen/bundles/robbyrussell/oh-my-zsh/themes/{agnoster.zsh-theme,agnoster.zsh-theme.antigen-compat}
+ln -s $shareENV/shell_config/{agnoster.zsh-theme,agnoster.zsh-theme.antigen-compat} ~/.antigen/bundles/robbyrussell/oh-my-zsh/themes
+
+# 安装autojump
+cd $my
+git clone git://github.com/joelthelion/autojump.git
+cd autojump
+./install.py
+cd ..
+rm autojump -rf
+
+
+# 安装vim 插件
+vim +BundleInstall +qall
+
+# 更换zsh
+chsh -s `which zsh`
+
