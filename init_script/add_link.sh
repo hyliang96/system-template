4c272615efceb1172121db0d0695849309bd9d8b
hyliang96 <hyliang96@hotmail.com>
2019-02-26 18:06:12 +0800

    modified add_link.sh, remove 'rm autojump'

--- init_script/add_link.sh
+++ init_script/add_link.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-rm /home/$USER/ENV/CONF/{.gitignore_global,.gitconfig,.ipython,.jupyter,.tmux.conf,.vimrc.bundles,.vim,.vimrc,.shadowsocks,.autojump,.env}
+rm /home/$USER/ENV/CONF/{.gitignore_global,.gitconfig,.ipython,.jupyter,.tmux.conf,.vimrc.bundles,.vim,.vimrc,.shadowsocks,.env}
 
 ln -s /home/$USER/ENV/shareENV/app_config/{.gitignore_global,.gitconfig,.ipython,.jupyter,.tmux.conf,vim/.vimrc.bundles,vim/.vim,vim/.vimrc} /home/$USER/ENV/CONF/
 
 
 ln -s /home/$USER/ENV/serverENV/app_config/.shadowsocks/ /home/$USER/ENV/CONF/
-ln -s /home/$USER/ENV/shareENV/shell_config/.autojump/ /home/$USER/ENV/CONF/
 ln -s /home/$USER/ENV/CONF/.shell_config/.env /home/$USER/ENV/CONF/
 
