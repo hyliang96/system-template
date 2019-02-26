0543eb043fcba6ff790707f8d0aac6662926a9a1
hyliang96 <hyliang96@hotmail.com>
2019-02-26 15:46:55 +0800

    modified add_link.sh

--- init_script/add_link.sh
+++ init_script/add_link.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+rm /home/$USER/ENV/CONF/{.gitignore_global,\
+.gitconfig, .ipython, .jupyter, .tmux.conf,\
+vim/.vimrc.bundles, vim/.vim, vim/.vimrc}
+
 ln -s /home/$USER/ENV/shareENV/app_config/{.gitignore_global,\
-.gitconfig, .ipython/, .jupyter/, .tmux.conf,\
-vim/.vimrc.bundles, vim/.vim/, vim/.vimrc} /home/$USER/ENV/CONF/
+.gitconfig, .ipython, .jupyter, .tmux.conf,\
+vim/.vimrc.bundles, vim/.vim, vim/.vimrc} /home/$USER/ENV/CONF/
 
 
 ln -s /home/$USER/ENV/serverENV/app_config/.shadowsocks/ /home/$USER/ENV/CONF/
