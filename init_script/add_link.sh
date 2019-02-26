212495455b3193818d9c725a917c7ae372422c73
hyliang96 <hyliang96@hotmail.com>
2019-02-26 16:09:06 +0800

    modified add_link.sh

--- init_script/add_link.sh
+++ init_script/add_link.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-rm /home/$USER/ENV/CONF/{.gitignore_global,\
-.gitconfig, .ipython, .jupyter, .tmux.conf,\
-vim/.vimrc.bundles, vim/.vim, vim/.vimrc}
+rm /home/$USER/ENV/CONF/{.gitignore_global,.gitconfig,.ipython,.jupyter,.tmux.conf,.vimrc.bundles,.vim,.vimrc,.shadowsocks,.autojump,.env}
 
-ln -s /home/$USER/ENV/shareENV/app_config/{.gitignore_global,\
-.gitconfig, .ipython, .jupyter, .tmux.conf,\
-vim/.vimrc.bundles, vim/.vim, vim/.vimrc} /home/$USER/ENV/CONF/
+ln -s /home/$USER/ENV/shareENV/app_config/{.gitignore_global,.gitconfig,.ipython,.jupyter,.tmux.conf,vim/.vimrc.bundles,vim/.vim,vim/.vimrc} /home/$USER/ENV/CONF/
 
 
 ln -s /home/$USER/ENV/serverENV/app_config/.shadowsocks/ /home/$USER/ENV/CONF/
-
+ln -s /home/$USER/ENV/shareENV/shell_config/.autojump/ /home/$USER/ENV/CONF/
+ln -s /home/$USER/ENV/CONF/.shell_config/.env /home/$USER/ENV/CONF/
 
