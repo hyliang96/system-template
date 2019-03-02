45fc1b90e6c3e2e49b16532421ced441ed43e8b6
hyliang96 <hyliang96@hotmail.com>
2019-03-03 05:07:19 +0800

    add notes on install_sys.sh

--- init_script/install_sys.sh
+++ init_script/install_sys.sh
@@ -17,6 +17,13 @@ bash init_script/add_link.sh
 url -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall +qall
+# 会自动安装各个插件，其中YouCompleteMe需要编译，会自动完成，编译无需sudo权限
+# 其编译依赖：cmake，build-essential，python-dev
+# 若缺，可根据报错情管理员安装之
+# sudo apt-get install cmake
+# sudo apt-get install build-essential
+# sudo apt-get install python python-dev
+# 然后重新`vim +PlugInstall +qall`，以编译YouCompleteMe
 
 # 更换zsh
 chsh -s `which zsh`
