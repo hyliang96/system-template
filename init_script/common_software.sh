bd2cf6945e79c0121d36ffd74dfdedad40cfab26
hyliang96 <hyliang96@hotmail.com>
2019-03-05 22:30:45 +0800

    add 'pip install nvim' to install python support for neovim

--- init_script/common_software.sh
+++ init_script/common_software.sh
@@ -5,6 +5,8 @@ bash Anaconda3-2018.12-Linux-x86_64.sh -p /home/${USER}/ENV/localENV/anaconda3 -
 # 默认环境是python3.6，pip=pip3=pip3.7
 pip install -U pip
 pip install shadowsocks
+# 安装neovim的python支持
+pip install neovim
 
 conda install -c bioconda p7zip --yes
 # 装 7z, 7za, 7zr ；-yes 表示不要问yes还是no，直接装
